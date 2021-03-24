af387b05e99f35864d29e6710cd810dcd450ce52
miiunii <dnflwlq3231@naver.com>
2021-03-24 15:57:00 +0900

    type: ADD body: CRA를 통한 react with typescript 세팅(eslint, prettier 등) 와 express js with typescript 세팅 완료

---
+++ server/src/index.ts
@@ -0,0 +1,63 @@
+import express from 'express';
+import createError from 'http-errors';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import session from 'express-session';
+import cors from 'cors';
+import FileStore = require('session-file-store');
+const useFileStore = FileStore(session);
+
+import { indexRouter } from './routes/index';
+
+const app = express();
+const PORT = 3001;
+
+app.set('views', path.join(__dirname, '../src/views'));
+app.set('view engine', 'ejs');
+
+app.use(cors());
+
+app.use(
+  session({
+    secret: 'omniapwd',
+    resave: false,
+    store: new useFileStore(),
+    cookie: {
+      httpOnly: false,
+      secure: false,
+      maxAge: 1200000,
+    },
+  })
+);
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err: any, req: any, res: any, next: any) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// start server at 3001
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
+
+module.exports = app;
---
+++ server/types/index.d.ts
@@ -0,0 +1,6 @@
+declare module 'http-errors';
+declare module 'cookie-parser';
+declare module 'morgan';
+declare module 'express-session';
+declare module 'session-file-store';
+declare module 'cors';
