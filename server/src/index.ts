cd0b5f21e555d33f0167ac8df29e0a59b2076d7d
miiunii <dnflwlq3231@naver.com>
2021-04-08 11:47:18 +0900

    heroku first commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -33,10 +33,10 @@ app.use(
   })
 );
 app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, '../../client/build')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../../client/build')));
 
 // Routing
 app.get('/', indexRouter);
