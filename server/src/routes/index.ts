be6c13970cf15658f1582b791d5317dc97a76a7a
miiunii <dnflwlq3231@naver.com>
2021-04-10 20:54:11 +0900

    TEST api

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -4,3 +4,9 @@ export const indexRouter = express.Router();
 indexRouter.get('/', (req, res) => {
   res.render('index.html');
 });
+
+indexRouter.post('/', (req, res) => {
+  res.json({
+    response: '자알왔다잉',
+  });
+});
